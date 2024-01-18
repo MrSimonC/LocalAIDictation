73f56d3fa1ad5a4c929b7c1fb7c5e478cc414c38
Simon Crouch <simon.crouch@malvernpanalytical.com>
2024-01-18 21:54:20 +0000

    code cleanup / rename vars

--- LocalAIDictationConsole/Program.cs
+++ LocalAIDictationConsole/Program.cs
@@ -10,15 +10,15 @@ string clipboardText = ClipboardService.GetText() ?? string.Empty;
 // Voice
 string initialWhisperPrompt = GetEnvironmentVariableFileContents("WHISPER_AI_INITIAL_PROMPT_PATH"); // 224 max tokens allowed by whisper ai
 ConversationContext? context = null;
-string foundContext = !string.IsNullOrEmpty(initialWhisperPrompt) ? " with context" : string.Empty;
-Console.WriteLine($"--- Recording{foundContext}. Press Space for dictation only, or any other to push to local AI... ---");
+string foundInitialWhisperPrompt = !string.IsNullOrEmpty(initialWhisperPrompt) ? " with context" : string.Empty;
+Console.WriteLine($"--- Recording{foundInitialWhisperPrompt}. Press Space for dictation only, or any other to push to local AI... ---");
 voiceToAi.VoiceInputRecordVoice();
 var keyPressed = Console.ReadKey(true);
 Console.WriteLine("--- Processing voice ---");
 string textDictation = await voiceToAi.VoiceProcessRecordingToTextAsync(initialWhisperPrompt);
 textDictation = textDictation.Replace("\n", " ");
 
-// write code to read in WHISPER_AI_POST_PROCESSING_PATH csv file using GetEnvironmentVariableFileContents(), ignore the header (first row), then loop through entires, replacing anything found in column 1 from the csv with column 2 from the csv in textDictation
+// read csv at path WHISPER_AI_POST_PROCESSING_PATH replacing anything found in column 1, with column 2
 string initialWhisperPromptCsv = GetEnvironmentVariableFileContents("WHISPER_AI_POST_PROCESSING_PATH");
 if (!string.IsNullOrEmpty(initialWhisperPromptCsv))
 {
@@ -32,7 +32,7 @@ if (!string.IsNullOrEmpty(initialWhisperPromptCsv))
 }
 else
 {
-    Console.WriteLine("--- No post processing file found ---");
+    Console.WriteLine("--- (No post processing file found) ---");
 }
 
 ClipboardService.SetText(textDictation);
@@ -45,23 +45,17 @@ if (keyPressed.Key == ConsoleKey.Spacebar)
 }
 
 // AI - Load Optional Context
-string initialPrompt = GetEnvironmentVariableFileContents("INITIAL_BASE_AI_CONTEXT_PATH");
-initialPrompt = string.Format(initialPrompt, clipboardText); // replace {0} in INITIAL_BASE_AI_CONTEXT_PATH file the with clipboard text
-if (string.IsNullOrEmpty(initialPrompt))
-{
-    Console.WriteLine("--- (Initial AI base context file *not* found or not specified) ---");
-}
+string initialBaseAIContext = GetEnvironmentVariableFileContents("INITIAL_BASE_AI_CONTEXT_PATH");
+initialBaseAIContext = string.Format(initialBaseAIContext, clipboardText); // replace {0} in INITIAL_BASE_AI_CONTEXT_PATH file the with clipboard text
+string foundInitialBaseAIContext = !string.IsNullOrEmpty(initialBaseAIContext) ? " with context" : string.Empty;
 #if DEBUG
-    Console.WriteLine($"---( Initial AI base context is: {initialPrompt} )---");
+    Console.WriteLine($"---( Initial AI base context is: {initialBaseAIContext} )---");
 #endif
-
-// AI - Call LLM
-Console.WriteLine("---Processing LLM. ---");
-(string streamedText, _) = await VoiceToAi.CallOllamaModelApi(initialPrompt + textDictation, context);
+Console.WriteLine($"--- Processing LLM {foundInitialBaseAIContext}---");
+(string streamedText, _) = await VoiceToAi.CallOllamaModelApi(initialBaseAIContext + textDictation, context);
 await ClipboardService.SetTextAsync(streamedText.Trim());
 Console.ReadKey(true);
 
-// create a local method which takes in an environment variable name which is a file path, reads the file if it exists, and returns either the file contents or an empty string
 static string GetEnvironmentVariableFileContents(string environmentVariableName)
 {
     string? filePath = Environment.GetEnvironmentVariable(environmentVariableName);
