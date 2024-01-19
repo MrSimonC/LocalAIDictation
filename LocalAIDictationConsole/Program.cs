0091f9f91511a551004f5160eca9c4fcdccfb611
Simon Crouch <simon.crouch@malvernpanalytical.com>
2024-01-19 16:21:17 +0000

    clean up: move env vars to top for clarity, allow {newline} in post processing file

--- LocalAIDictationConsole/Program.cs
+++ LocalAIDictationConsole/Program.cs
@@ -2,57 +2,58 @@
 using OllamaSharp;
 using TextCopy;
 
+// Environment Variables
+string whisperInitialPrompt = GetEnvironmentVariableFileContents("WHISPER_AI_INITIAL_PROMPT_PATH");
+string whisperPostProcessingCsv = GetEnvironmentVariableFileContents("WHISPER_AI_POST_PROCESSING_PATH");
+string initialBaseAIContext = GetEnvironmentVariableFileContents("OLLAMA_BASE_CONTEXT_PATH");
+string? model = Environment.GetEnvironmentVariable("OLLAMA_MODEL") ?? "phi";
+
 var voiceToAi = new VoiceToAi();
 
 // Get Clipboard before recording
 string clipboardText = ClipboardService.GetText() ?? string.Empty;
 
 // Voice
-string initialWhisperPrompt = GetEnvironmentVariableFileContents("WHISPER_AI_INITIAL_PROMPT_PATH"); // 224 max tokens allowed by whisper ai
 ConversationContext? context = null;
-string foundInitialWhisperPrompt = !string.IsNullOrEmpty(initialWhisperPrompt) ? " with context" : string.Empty;
-Console.WriteLine($"--- Recording{foundInitialWhisperPrompt}. Press Space for dictation only, or any other key to use local AI ---");
+string foundInitialWhisperPrompt = !string.IsNullOrEmpty(whisperInitialPrompt) ? "with" : "without";
+string foundWhisperPostProcessingCsv = !string.IsNullOrEmpty(whisperPostProcessingCsv) ? "with" : "without";
+Console.WriteLine($"--- Recording {foundInitialWhisperPrompt} context {foundWhisperPostProcessingCsv} post processing ---");
+Console.WriteLine("Press Space for dictation only, or any other key to use local AI ---");
 voiceToAi.VoiceInputRecordVoice();
 var keyPressed = Console.ReadKey(true);
 Console.WriteLine("--- Processing voice ---");
-string textDictation = await voiceToAi.VoiceProcessRecordingToTextAsync(initialWhisperPrompt);
-textDictation = textDictation.Replace("\n", " ");
+string textDictation = await voiceToAi.VoiceProcessRecordingToTextAsync(whisperInitialPrompt);
 
-// read csv at path WHISPER_AI_POST_PROCESSING_PATH replacing anything found in column 1, with column 2
-string initialWhisperPromptCsv = GetEnvironmentVariableFileContents("WHISPER_AI_POST_PROCESSING_PATH");
-if (!string.IsNullOrEmpty(initialWhisperPromptCsv))
+if (!string.IsNullOrEmpty(whisperPostProcessingCsv))
 {
-    Console.WriteLine("--- Performing Post Processing ---");
-    string[] initialWhisperPromptCsvLines = initialWhisperPromptCsv.Split("\r\n");
+    string[] initialWhisperPromptCsvLines = whisperPostProcessingCsv.Split("\r\n");
     for (int i = 1; i < initialWhisperPromptCsvLines.Length; i++)
     {
         string[] initialWhisperPromptCsvLine = initialWhisperPromptCsvLines[i].Split(",");
-        textDictation = textDictation.Replace(initialWhisperPromptCsvLine[0], initialWhisperPromptCsvLine[1]);
+        string firstColumnValue = initialWhisperPromptCsvLine[0];
+        if (firstColumnValue == "{newline}") // special self-made pattern representing a new line
+        {
+            firstColumnValue = "\n";
+        }
+        textDictation = textDictation.Replace(firstColumnValue, initialWhisperPromptCsvLine[1]);
     }
 }
-else
-{
-    Console.WriteLine("--- (No post processing file found) ---");
-}
-
+Console.WriteLine("--- Processing done. You said ---");
+Console.WriteLine(textDictation);
 ClipboardService.SetText(textDictation);
 Console.WriteLine("--- Dictation copied to clipboard ---");
-// if keyPressed = space, then exit the program
 if (keyPressed.Key == ConsoleKey.Spacebar)
 {
-    Console.WriteLine("--- Exiting program ---");
     return;
 }
 
 // AI - Load Optional Context
-string initialBaseAIContext = GetEnvironmentVariableFileContents("INITIAL_BASE_AI_CONTEXT_PATH");
 initialBaseAIContext = string.Format(initialBaseAIContext, clipboardText); // replace {0} in INITIAL_BASE_AI_CONTEXT_PATH file the with clipboard text
 string foundInitialBaseAIContext = !string.IsNullOrEmpty(initialBaseAIContext) ? "with context " : string.Empty;
 #if DEBUG
-    Console.WriteLine($"---( Initial AI base context is: {initialBaseAIContext} )---");
+Console.WriteLine($"---( Initial AI base context is: {initialBaseAIContext} )---");
 #endif
 
-string? model = Environment.GetEnvironmentVariable("OLLAMA_MODEL") ?? "phi";
 Console.WriteLine($"--- Processing {model} LLM {foundInitialBaseAIContext}---");
 string prompt = initialBaseAIContext + textDictation;
 (string streamedText, _) = await VoiceToAi.CallOllamaModelApi(model, prompt, context);
